7c8456896498f70f1958d95efb7d921dda5de639
ntsang2511 <tansang25112003@gmail.com>
2024-04-08 00:29:10 +0700

    xu ly nhan viec va sua database

--- QuanLyKhachSan/QuanLyKhachSan/frmNhanVien.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/frmNhanVien.Designer.cs
@@ -29,10 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dtgv = new System.Windows.Forms.DataGridView();
+            this.btnHoanThanh = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMaPhong = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -45,49 +47,71 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Việc phải làm";
             // 
-            // dataGridView1
+            // dtgv
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(59, 107);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(665, 198);
-            this.dataGridView1.TabIndex = 1;
+            this.dtgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv.Location = new System.Drawing.Point(59, 107);
+            this.dtgv.Name = "dtgv";
+            this.dtgv.RowHeadersWidth = 51;
+            this.dtgv.RowTemplate.Height = 24;
+            this.dtgv.Size = new System.Drawing.Size(665, 198);
+            this.dtgv.TabIndex = 1;
+            this.dtgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgv_CellContentClick);
             // 
-            // button1
+            // btnHoanThanh
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(59, 356);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(190, 72);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Hoàn thành";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnHoanThanh.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHoanThanh.Location = new System.Drawing.Point(59, 337);
+            this.btnHoanThanh.Name = "btnHoanThanh";
+            this.btnHoanThanh.Size = new System.Drawing.Size(190, 72);
+            this.btnHoanThanh.TabIndex = 2;
+            this.btnHoanThanh.Text = "Hoàn thành";
+            this.btnHoanThanh.UseVisualStyleBackColor = true;
+            this.btnHoanThanh.Click += new System.EventHandler(this.btnHoanThanh_Click);
             // 
-            // button2
+            // btnHuy
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(534, 356);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(190, 72);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Hủy";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(534, 337);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(190, 72);
+            this.btnHuy.TabIndex = 3;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(230, 431);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Phòng đang chọn";
+            // 
+            // txtMaPhong
+            // 
+            this.txtMaPhong.Location = new System.Drawing.Point(387, 433);
+            this.txtMaPhong.Name = "txtMaPhong";
+            this.txtMaPhong.Size = new System.Drawing.Size(146, 22);
+            this.txtMaPhong.TabIndex = 5;
             // 
             // frmNhanVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 472);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtMaPhong);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnHoanThanh);
+            this.Controls.Add(this.dtgv);
             this.Controls.Add(this.label1);
             this.Name = "frmNhanVien";
             this.Text = "frmNhanVien";
             this.Load += new System.EventHandler(this.frmNhanVien_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,8 +120,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dtgv;
+        private System.Windows.Forms.Button btnHoanThanh;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMaPhong;
     }
 }
--- QuanLyKhachSan/QuanLyKhachSan/frmNhanVien.cs
+++ QuanLyKhachSan/QuanLyKhachSan/frmNhanVien.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,20 @@ namespace QuanLyKhachSan
 {
     public partial class frmNhanVien : Form
     {
+        SqlConnection connection;
+        SqlCommand command;
+        string str = "Data Source=SANG-ADVICE\\SQLEXPRESS;Initial Catalog=QuanLyKhachSan;Integrated Security=True";
+        SqlDataAdapter adapter;
+        DataTable table = new DataTable();
+        void loadData()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "select MAPHONG, TENNV, CHUCVU from NHANVIEN inner join PHONG on NHANVIEN.MADV = PHONG.MADV and TINHTRANGDICHVU = 0";
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            dtgv.DataSource = table;
+        }
         public frmNhanVien()
         {
             InitializeComponent();
@@ -24,7 +39,33 @@ namespace QuanLyKhachSan
 
         private void frmNhanVien_Load(object sender, EventArgs e)
         {
+            connection = new SqlConnection(str);
+            connection.Open();
+            loadData();
+        }
 
+        private void btnHoanThanh_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "update PHONG set TINHTRANGDICHVU = 1";
+            command.ExecuteNonQuery();
+            loadData();
+        }
+
+        private void dtgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMaPhong.ReadOnly = true;
+            int i;
+            i = dtgv.CurrentRow.Index;
+            txtMaPhong.Text = dtgv.Rows[i].Cells[0].Value.ToString();
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "delete from PHONG where MAPHONG = '" + txtMaPhong.Text + "'";
+            command.ExecuteNonQuery();
+            loadData();
         }
     }
 }
